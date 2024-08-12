74d34bf5bc2a57b2348b82bd32f0361bc58254b3
Rishabh Kumar <110777053+rishabh5799kumar@users.noreply.github.com>
2024-08-12 10:13:27 +0530

    Add files via upload

---
+++ a_rtchat/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+import shortuuid
+
+# Create your models here.
+class ChatGroup(models.Model):
+    group_name = models.CharField(max_length=128, unique=True, blank=True, default=shortuuid.uuid)
+    groupchat_name = models.CharField(max_length=128, null=True, blank=True)
+    users_online = models.ManyToManyField(User, related_name='online_in_groups', blank=True)
+
+    members = models.ManyToManyField(User, related_name='chat_groups', blank=True)
+    is_private = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.group_name
+
+    
+
+class GroupMessage(models.Model):
+    group = models.ForeignKey(ChatGroup, related_name='chat_messages', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.CharField(max_length=300, blank=True, null=True)
+    file = models.FileField(upload_to='files/', blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.body:
+            return f'{self.author.username} : {self.body}'
+        elif self.file:
+            return f'{self.author.username} : {self.filename}'
+    
+    class Meta:
+        ordering = ['-created']
